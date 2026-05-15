9cb57087f2a5722faa0c86a76ee1f557488ccac1
Mryanto96 <97yanto.aurelius@gmail.com>
2026-05-15 10:37:43 +0200

    komit

--- js/api.js
+++ js/api.js
@@ -1,2 +1,2 @@
 ﻿// API Integration dengan Google Apps Script
-const API_URL = 'https://script.google.com/macros/s/.../exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxswCblegBjQI26qAnb-aczydYO4sYkuHPlSthwUISn9pUrrGkYuA8vu10N-YFjVH1J/exec';
